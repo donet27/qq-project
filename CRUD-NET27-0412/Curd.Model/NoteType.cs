24b48b0e3ef893d9b003c4d172be9f4728aaa4d7
373376253 <373376253@users.noreply.github.com>
2019-06-04 11:37:47 +0800

    1
    1

---
+++ CRUD-NET27-0412/Curd.Model/NoteType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud.Model
+{
+    public class NoteType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Note> Notes { get; set; }
+    }
+}
